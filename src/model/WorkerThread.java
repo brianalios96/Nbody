28e7b4215bd8a17dc53835b2e4769da7cdef0987
Isaac Plunkett <isaacp@email.arizona.edu>
2017-11-04 20:56:12 -0700

    added a worker thread

---
+++ src/model/WorkerThread.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.concurrent.*;
+
+public class WorkerThread extends Thread
+{
+	private int threadID;
+	private int numThread;
+	private static Semaphore barriers[][];
+
+	public WorkerThread(int ID, int numThreads)
+	{
+		threadID = ID;
+		this.numThread = numThreads;
+		
+		if(barriers == null)
+		{
+			barriers = new Semaphore[5][32];
+			for(int i = 0; i < barriers.length; i++)
+			{
+				for(int j = 0; j < barriers[0].length; j++)
+				{
+					barriers[i][j] = new Semaphore(0);
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void run()
+	{
+		
+	}
+	
+	private void barrier()
+	{
+		int i = 0;
+		for (int stage = 1; stage < numThread; stage*=2)
+		{
+			barriers[i][threadID].release();
+			int nextID = (threadID + stage) % numThread;
+			try
+			{
+				barriers[i][nextID].acquire();
+			} catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+			i++;
+		}
+
+	}
+}

8ee5c19ea364454e46b3bd0db5cba72aa8019393
BrianaL <brianalios96@email.arizona.edu>
2017-11-04 20:51:32 -0700

    Created NBodyParallel

--- src/model/NBody.java
+++ src/model/NBody.java
@@ -62,19 +62,7 @@ public class NBody
 			allBodies[i].setSize(size);
 			}
 		}
-		// Random rng = new Random();
 
-//		allBodies[0].setxPosition(200);
-//		allBodies[0].setyPosition(150);
-//		allBodies[0].setxVelocity(-0.1);
-//		allBodies[0].setyVelocity(0);
-//		allBodies[0].setSize(size);
-//
-//		allBodies[1].setxPosition(100);
-//		allBodies[1].setyPosition(150);
-//		allBodies[1].setxVelocity(0.1);
-//		allBodies[1].setyVelocity(0);
-//		allBodies[1].setSize(size);
 		NBodyGUI gui = null;
 		if (guiOn) {
 			//second parameter is the window size
